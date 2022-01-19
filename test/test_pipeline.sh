1ddb4fa306ebd957525a3425fc3257d8dbe695b7
Rob Vickerstaff <robert.vickerstaff@gmail.com>
2022-01-19 17:36:04 +0000

    added test pipeline comments

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -12,12 +12,17 @@ export PATH=${KMERZDIR}/scripts:${PATH}
 
 BASENAME=10k
 
+#create a simple random "genome" sequence
 random_sequence.py seq 1 10000 0 > ${BASENAME}.flat
 
+#simulate reads from the genome
 generate_reads.py ${BASENAME}.flat 20000 100 0.01 0.01 0.5 > ${BASENAME}_reads.flat
 
+#count kmers in the reads
 count_kmers.py ${BASENAME}_reads.flat 23 > ${BASENAME}_counts
 
+#assemble the kmers into contigs
 assemble_kmers.py ${BASENAME}_counts 30 > ${BASENAME}_assembly.flat
 
+#generate a dot plot comparing kmer positions in original and assembled sequence
 compare_kmers.py ${BASENAME}_assembly.flat ${BASENAME}.flat 10 > ${BASENAME}_dots
