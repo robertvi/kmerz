126ae17dc26016eff9ab32237d8a0b740c8e16a4
Rob Vickerstaff <robert.vickerstaff@gmail.com>
2022-01-21 00:03:37 +0000

    pull in string2uint64t function from popseq, to convert to integer representation of kmer string

--- src/kmerz.cpp
+++ src/kmerz.cpp
@@ -5,6 +5,9 @@
 
 namespace kmerz
 {
+const uint64_t KMER_SIZE=31;
+const uint64_t KMER_MASK=( ((uint64_t)1)<<(KMER_SIZE*2) )-1;
+const uint64_t KMER_FIRSTOFFSET=(KMER_SIZE-1)*2;
 
 EulerGraph::EulerGraph(const std::string&inputFile,int minKmerCount)
 {
@@ -12,14 +15,15 @@ EulerGraph::EulerGraph(const std::string&inputFile,int minKmerCount)
     ifs.open (inputFile, std::ios::binary);
 
     std::string fwd_kmer,rev_kmer;
-    int line_ctr=0,count;
+    int line_ctr=0;
+    uint64_t count;
 
     while(ifs.good())
     {
         line_ctr++;
 
         ifs >> fwd_kmer;
-        
+
         if(ifs.eof()) break;
 
         if(ifs.fail())
@@ -51,11 +55,57 @@ EulerGraph::EulerGraph(const std::string&inputFile,int minKmerCount)
             throw std::runtime_error("Error: invalid kmer count on line: " + std::to_string(line_ctr) + ": count was " + std::to_string(count));
         }
 
-        std::cout << fwd_kmer << " " << rev_kmer << " " << count << std::endl;
+        if(count < minKmerCount) continue;
+
+        //convert kmer to uint64_t representation
+        uint64_t canonical_kmer = string2uint64t(fwd_kmer.c_str());
+        kmer_list.push_back( canonical_kmer );
+
+        std::cout << fwd_kmer << " " << canonical_kmer << " " << count << std::endl;
     }
 
     ifs.close();
 }
 
+//converts to uint64_t and ensures its canonical
+uint64_t string2uint64t(const std::string&seq)
+{
+    uint64_t fkmer=0,rkmer=0;
+
+    for(auto p=0; p<KMER_SIZE; p++)
+    {
+        //fkmer: grows from the right (LSB)
+        //rkmer: grows from the left (MSB)
+        switch(seq[p])
+        {
+            case 'A':
+            case 'a':
+                fkmer = ((fkmer << 2) +  uint64_t(0)) & KMER_MASK;
+                rkmer =  (rkmer >> 2) + (uint64_t(3) << KMER_FIRSTOFFSET);
+                break;
+            case 'C':
+            case 'c':
+                fkmer = ((fkmer << 2) +  uint64_t(1)) & KMER_MASK;
+                rkmer =  (rkmer >> 2) + (uint64_t(2) << KMER_FIRSTOFFSET);
+                break;
+            case 'G':
+            case 'g':
+                fkmer = ((fkmer << 2) +  uint64_t(2)) & KMER_MASK;
+                rkmer =  (rkmer >> 2) + (uint64_t(1) << KMER_FIRSTOFFSET);
+                break;
+            case 'T':
+            case 't':
+                fkmer = ((fkmer << 2) +  uint64_t(3)) & KMER_MASK;
+                rkmer =  (rkmer >> 2) + (uint64_t(0) << KMER_FIRSTOFFSET);
+                break;
+            default:
+                throw std::runtime_error("Error: invalid character in kmer: " + seq[p]);
+        }
+    }
+
+    //return canonical version
+    if (fkmer <= rkmer) return fkmer;
+    else                return rkmer;
+}
 
 } //namespace kmerz
--- src/kmerz.h
+++ src/kmerz.h
@@ -2,14 +2,22 @@
 #define __ROBERTVI_KMERZ_KMERZ_H__
 
 #include <string>
+#include <vector>
+
+//#define KMERSIZE 31
 
 namespace kmerz
 {
     class EulerGraph
     {
         public:
+            //load kmer sequences and counts from file
             EulerGraph(const std::string&inputFile,int minKmerCount);
         private:
+            //simple list of canonical kmers
+            std::vector< uint64_t > kmer_list;
     };
+
+    uint64_t string2uint64t(const std::string&);
 } //namespace kmerz
 #endif //__ROBERTVI_KMERZ_KMERZ_H__
