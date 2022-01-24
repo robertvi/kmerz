23edd2a94f0cf99f81b4c4498d1eb9112f733bb4
Rob Vickerstaff <robert.vickerstaff@gmail.com>
2022-01-24 19:11:42 +0000

    tweaked build_and_test.s ready for test install from a git clone

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# build the main binary and the test binary
+# then run the test binary and report "okay" if all test were passed
+#
+# you can use the Makefile if you only want to build the main binary (kmerz)
+#
+
 set -eu
 
 g++ -g -o kmerz \
